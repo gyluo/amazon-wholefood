3ed09e9cb108741c0f1525f6cf141ffc1644addd
gyluo <guoying.luo@gmail.com>
2020-04-05 16:44:55 -0400

    Update WebdriverSettings.java

--- src/test/java/testcases/settings/WebdriverSettings.java
+++ src/test/java/testcases/settings/WebdriverSettings.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "/Users/gluo/Work/selenium/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/<path>/chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("headless");
 		options.addArguments("window-size=1200x600");
